615946be0e714c72b01e9c6d66ab33f3832ad47d
zohra-neda <zohra.neda@gmail.com>
2023-12-16 13:29:05 +0430

    Implement Search component with dropdowns and search button

---
+++ src/components/Search.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// import components
+import CountryDropdown from './CountryDropdown';
+import PropertyDropdown from './PropertyDropdown';
+import PriceRangeDropdown from './PriceRangeDropdown';
+
+// import icons
+import { RiSearch2Line } from 'react-icons/ri';
+
+const Search = () => {
+  return (
+    <div className='px-[30px] py-6 max-w-[1170px] mx-auto flex-col lg:flex-row justify-between gap-4 lg:gap-x-3
+    relative lg:-top-4 lg:shadow-1 bg-white rounded-lg lg:bg-transparent lg:backdrop:blur'>
+      <CountryDropdown />
+      <PropertyDropdown />
+      <PriceRangeDropdown />
+      <button className='bg-violet-700 hover:bg-violet-800 transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center items-center text-white text-lg'>
+        <RiSearch2Line />
+      </button>
+    </div>
+  );
+};
+
+export default Search;
