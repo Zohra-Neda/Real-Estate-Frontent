378a17fa75e9375808618f6d2418fa1fe2a83b7a
zohra-neda <zohra.neda@gmail.com>
2023-12-18 14:14:43 +0430

    Import and add HouseList component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 // import components
 import Banner from '../components/Banner';
+import HouseList from '../components/HouseList';
 
 const Home = () => {
   return <div className='min-h-[1800px]'>
-    <Banner/>
+    <Banner />
+    <HouseList />
   </div>;
 };
 
