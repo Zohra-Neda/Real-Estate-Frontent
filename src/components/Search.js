5184a31e486ce017354a0300f68d66242fe09150
zohra-neda <zohra.neda@gmail.com>
2023-12-18 14:12:01 +0430

    Destructure handleClick from HouseContext

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 // import components
 import CountryDropdown from './CountryDropdown';
@@ -8,14 +8,20 @@ import PriceRangeDropdown from './PriceRangeDropdown';
 // import icons
 import { RiSearch2Line } from 'react-icons/ri';
 
+// import context
+import { HouseContext } from './HouseContext';
+
 const Search = () => {
+  // const { houses } = useContext(HouseContext);
+  // console.log(houses);
+  const { handleClick } = useContext(HouseContext);
   return (
-    <div className='px-[30px] py-6 max-w-[1170px] mx-auto flex-col lg:flex-row justify-between gap-4 lg:gap-x-3
+    <div className='px-[30px] py-6 max-w-[1170px] mx-auto flex flex-col lg:flex-row justify-between gap-4 lg:gap-x-3
     relative lg:-top-4 lg:shadow-1 bg-white rounded-lg lg:bg-transparent lg:backdrop:blur'>
       <CountryDropdown />
       <PropertyDropdown />
       <PriceRangeDropdown />
-      <button className='bg-violet-700 hover:bg-violet-800 transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center items-center text-white text-lg'>
+      <button onClick={() => handleClick()} className='bg-violet-700 hover:bg-violet-800 transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center items-center text-white text-lg'>
         <RiSearch2Line />
       </button>
     </div>
